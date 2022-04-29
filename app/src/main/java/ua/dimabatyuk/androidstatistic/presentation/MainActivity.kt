826619314ffe1f79261bdeba9a7bd9310c92f844
Dima Batyuk <dimabatyuk@gmail.com>
2022-04-29 19:56:52 +0300

    changed app name and logo, make better readability of Android ID

--- app/src/main/java/ua/dimabatyuk/androidstatistic/presentation/MainActivity.kt
+++ app/src/main/java/ua/dimabatyuk/androidstatistic/presentation/MainActivity.kt
@@ -2,11 +2,16 @@ package ua.dimabatyuk.androidstatistic.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
 import ua.dimabatyuk.androidstatistic.R
+import java.lang.Integer.min
 
 class MainActivity : AppCompatActivity() {
     private val viewModel: MainViewModel by viewModels()
@@ -21,6 +26,27 @@ class MainActivity : AppCompatActivity() {
             viewModel.state
                 .collect {
                     androidIdValue.text = it.androidId.toString()
+                        .let {
+
+                            val step = 3
+                            val sp = SpannableString(it)
+                            for (i: Int in 0..it.length step step) {
+                                val color = if (i % 2 == 0) {
+                                    ForegroundColorSpan(getColor(R.color.black))
+                                } else {
+                                    ForegroundColorSpan(getColor(R.color.dark_grey))
+                                }
+
+                                sp.setSpan(
+                                    color,
+                                    i,
+                                    min(i+step, it.length),
+                                    Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                                    )
+                            }
+
+                            sp
+                        }
                 }
         }
     }
